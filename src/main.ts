8b0fe8d2afbc0717592d40645bc36483a6c011ff
nalberthy <nalberthyuf@gmail.com>
2024-12-08 01:39:59 -0300

    feat: configure cors and definition port of env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
+  const { PORT, CORS_ALLOWED_ORIGINS } = process.env;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = PORT ?? 3000;
+
+  const corsOptions: CorsOptions = {
+    origin: CORS_ALLOWED_ORIGINS?.split(','),
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+  app.listen(port).then(() => {
+    logger.log(`Running server in port ${port}`);
+  });
 }
 bootstrap();
