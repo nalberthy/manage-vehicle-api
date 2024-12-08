c741787d6753c39ed47773163d404fd89b1f0968
nalberthy <nalberthyuf@gmail.com>
2024-12-08 01:43:33 -0300

    feat: soft middleware to control prisma

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -7,7 +7,54 @@ import { PrismaService } from './prisma.service';
     {
       provide: PrismaService,
       useFactory: async () => {
-        const prisma = new PrismaService();
+        const prisma = new PrismaService().$extends({
+          query: {
+            $allModels: {
+              $allOperations({ operation, args, query }) {
+                // Updated at
+                if (operation === 'update' || operation === 'updateMany') {
+                  args['updatedAt'] = new Date();
+                }
+
+                // Soft delete
+                if (
+                  operation === 'findMany' ||
+                  operation === 'findFirst' ||
+                  operation === 'count' ||
+                  operation === 'aggregate'
+                ) {
+                  if (args === undefined) {
+                    args = {};
+                  }
+
+                  if (args['where'] !== undefined) {
+                    if (args['where'].deletedAt === undefined) {
+                      args['where']['deletedAt'] = null;
+                    }
+                  } else {
+                    args['where'] = { deletedAt: null };
+                  }
+                }
+
+                if (operation === 'delete') {
+                  operation = 'update';
+                  args['data'] = { deletedAt: new Date() };
+                }
+
+                if (operation === 'deleteMany') {
+                  // Delete many queries
+                  operation = 'updateMany';
+                  if (args['data'] !== undefined) {
+                    args['data']['deletedAt'] = new Date();
+                  } else {
+                    args['data'] = { deletedAt: new Date() };
+                  }
+                }
+                return query(args);
+              },
+            },
+          },
+        });
         await prisma.$connect();
         return prisma;
       },
